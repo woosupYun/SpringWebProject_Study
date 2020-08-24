a1dea05c0741a9a50d0e0ce22796a7b6e20a7b04
woosupyun <yunwoosup@naver.com>
2020-08-25 00:34:37 +0900

    1. [xml] 설정 버전 커넥션 풀 test 2. [java] 설정 버전 커넥션 풀 test

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -1,10 +1,32 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages = "org.zerock.sample")
-public class RootConfig {
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
+@Configuration
+@ComponentScan(basePackages = {"org.zerock.sample"})
+public class RootConfig {
+	@Bean
+	public HikariConfig hikariConfig() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+//		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+//		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		return hikariConfig;
+	}
+
+	@Bean(destroyMethod = "close")
+	public DataSource DataSource(HikariConfig hikariConfig) {
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		return dataSource;
+	}
 }
