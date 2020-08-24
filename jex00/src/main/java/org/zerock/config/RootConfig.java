b8fbe730fc6e77a9ef1de119344301084abc1626
woosupyun <yunwoosup@naver.com>
2020-08-25 01:26:53 +0900

    1. xml 설정 SQL문 Mapper test 2가지방식  (select 어노테이션 , xml 설정 )
    2. java 설정 SQL문 Mapper test 2가지방식  (select 어노테이션 , xml 설정 )

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -3,6 +3,7 @@ package org.zerock.config;
 import javax.sql.DataSource;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
 public class RootConfig {
 	@Bean
 	public HikariConfig hikariConfig() {
